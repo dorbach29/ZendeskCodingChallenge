2cce05cb452ba2987149984697269f93bdb8e039
Daniel Orbach <danielorbach@Daniels-MacBook-Air-2.local>
2021-11-30 19:23:56 -0500

    getAllTickets modified to fetch users

--- ZendeskHandler.js
+++ ZendeskHandler.js
@@ -8,36 +8,28 @@ Module that handles calls to the Zendesk API
 */
 
 /*
--Get list of all tickets 
-    https://zccdanielorbach.zendesk.com/api/v2/incremental/tickets?start_time=0
-
-    using cursor:
-    https://zccdanielorbach.zendesk.com/api/v2/incremental/tickets/cursor.json?start_time=0
-    --If there are more to see it will inform us
 -Get info for individual ticket
     https://zccdanielorbach.zendesk.com/api/v2/tickets/91.json
     91 is the ticket id
 
 
--Get User Info
-    https://zccdanielorbach.zendesk.com/api/v2/users/0.json
-    Where 0 is the id
-    return 404 when no user is found 
-*/
 
 
 
-/*
-Wrapper for https.get method 
-Params: Url, Options
-Returns a promise that resolves with:
-Error
-Response : {
-    statusCode
-    headers
-    body
-}
-*/
+/**
+ * Basic Wrapper for https.get method 
+ * Params: Url, Options
+ * Returns a promise that resolves with:
+ * Error or
+ * Response : {
+ *  statusCode
+ *  headers
+ *  body
+ * }
+ * 
+ */
+
+
 function asyncGet(url){
     const options =  { headers : { 
         'Authorization': 'Basic ' + Buffer.from(process.env.ZEN_USER + ':' + process.env.ZEN_PASS).toString('base64'),
@@ -64,11 +56,71 @@ function asyncGet(url){
     })
 }
 
+/**
+ * Returns the info corresponding to an individual ticket 
+ * 
+ */
+//TODO: Handle for NUll case when no ticket is returned
+async function getTicketInfo(ticketId){
+    const val = {
+        error : "none",
+        statusCode : 200,
+        ticket: null,
+    }
+
+    const url = `https://${SUBD}.zendesk.com/api/v2/tickets/${ticketId}.json`
+    try{
+        let response = await asyncGet(url);
+        if(response.statusCode != 200){
+            val.statusCode = response.statusCode;
+            val.error = response.body.error
+            return val;
+        }
+        val.ticket = response.body;
+        return val;
+    } catch (err){
+        console.log(err)
+        val.error = err;
+        return val; 
+    }
+}
+
+/**
+ * Returns the ticket count in the users account
+ * Accessed via val.ticketCount
+ */
+async function getTicketCount(){
+    const val = {
+        error : "none",
+        statusCode : 200,
+        ticketCount : 0,
+    }
+
+    const url = `https://${SUBD}.zendesk.com/api/v2/users/me.json?include=open_ticket_count`
+    try{
+        let response = await asyncGet(url);
+        if(response.statusCode != 200){
+            val.statusCode = response.statusCode;
+            val.error = response.body.error
+            return val;
+        }
+        val.ticketCount = response.body.open_ticket_count[response.body.user.id];
+        return val;
+    } catch (err){
+        console.log(err)
+        val.error = err;
+        return val; 
+    }
+}
+
 
 /*
-    Grabs all tickets for a Zendesk account, takes paging into account 
+    Grabs all tickets for a Zendesk account, takes paging into account .
+    Sideloads users associated with these tickets.
     Returns  {
         error: appropriate error (or null)
+        statusCode : statusCode of http response
+        users: map containing user_id's as keys and userinfo as responses
         tickets: list of tickets
     }
 */
@@ -78,8 +130,9 @@ async function getAllTickets(){
         error : "none",
         statusCode : 200,
         tickets : [], 
+        users : new Map(),
     }
-    let nextUrl = `https://${SUBD}.zendesk.com/api/v2/incremental/tickets/cursor.json?start_time=0&per_page=100`;
+    let nextUrl = `https://${SUBD}.zendesk.com/api/v2/incremental/tickets/cursor.json?start_time=0&per_page=100&include=users`;
     try {
         let cont = true; 
         while(cont){
@@ -89,10 +142,17 @@ async function getAllTickets(){
                 val.error = response.body.error
                 return val;
             };
-            //Checking if there are
+            //Checking if there are more ticketss
             cont = !response.body.end_of_stream;
             nextUrl = response.body.after_url; 
+
+            //Pushing Tickets and adidng users
             val.tickets.push(...response.body.tickets);
+            for(let i = 0; i < response.body.users.length; i ++){
+                let user = response.body.users[i];
+                val.users.set(user.id, user);
+            }
+
         }
         return val;
     } catch (err){
@@ -104,16 +164,18 @@ async function getAllTickets(){
 }
 
 
+//Dummy used for testing 
+async function f() {
+    let data = await getTicketInfo(91);
+    console.log(data.ticket);
+}
 
-
-
-
-
-
-
+//f();
 
 
 module.exports = {
     getAllTickets,
+    getTicketCount,
+    getTicketInfo,
     asyncGet,
 }
--- test/ZendeskHandler.test.js
+++ test/ZendeskHandler.test.js
@@ -1,40 +1,23 @@
-const {getAllTickets, asyncGet} = require("../ZendeskHandler");
-
-/*
-ACCOUNT SPECIFIC TESTING VARIABLES
+const {getAllTickets,  getTicketCount, asyncGet} = require("../ZendeskHandler");
 
 
-expectedTicketCount - total tickets on a zendesk account;
 
-user : {
-    user_name : existing user's user_name
-    user_id : existing user (same as above) users id
-}
-
-*/
-const expectedTicketCount = 101;
-const user =  {
-    user_name : "Daniel Orbach" , 
-    user_id : "1267161330189"
-}
 
 
 /* TESTS */
 
 
 
-//Check correct user is returned 
 
-//Check ticket count 
-jest.setTimeout(10000);
+//Check that all tickets are returne by getAllTickets 
+jest.setTimeout(15000);
 test("Number of tickets returned", async () => {
-   let data = await getAllTickets();
-   expect(data.error).toBe("none");
-   expect(data.tickets.length).toBe(expectedTicketCount);
+    let ticketCount = await getTicketCount()
+    ticketCount = ticketCount.ticketCount;
+    let data = await getAllTickets();
+    expect(data.error).toBe("none");
+    expect(data.tickets.length).toBe(ticketCount);
 })
 jest.setTimeout(5000);
 
 
-//Check no user is returned if it gives wrong user
-
-//Check last ticket
